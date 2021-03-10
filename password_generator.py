7b932de7d91a8e365e91a73af93cc1b838cff7a8
lunAr-creator <lunArcreator3@gmail.com>
2021-03-10 12:59:28 +0000

    Updated password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,32 +1,41 @@
 import random
 import string
 
-class password:
+class simplepass:
+    def __init__(self, length, characters):
+        self.length = length
+        self.characters = characters
+
+    def generate(self, iterations):
+        for p in range(iterations):
+            output_password = ''
+            for c in range(self.length):
+                output_password += random.choice(self.characters)
+            print(output_password)
+
+class complexpass(simplepass):
 	def __init__(self, length, string_method, numbers=True, special_chars=False):
 		self.length = length
 		self.string_method = string_method
 		self.numbers = numbers
 		self.special_chars = special_chars
 
-	def generate_intricate(self, iterations):
-		if self.string_method == 'upper':
-			stringMethod = string.ascii_uppercase
-		elif self.string_method == 'lower':
-			stringMethod = string.ascii_lowercase
-		elif self.string_method == 'both':
-			stringMethod = string.ascii_letters
+	def generate(self, iterations):
+		characters = ''
+
+		methods = {
+    		'upper': string.ascii_uppercase,
+    		'lower': string.ascii_lowercase,
+    		'both': string.ascii_letters
+		}
+
+		characters += methods[self.string_method]
 
 		if self.numbers:
-			stringNumbers = string.digits
-		elif self.numbers:
-			stringNumbers = ''
+			characters += string.digits
+		if self.special_chars:
+			characters += string.punctuation
 
-		if self.special_chars == True:
-			stringSpecial = string.punctuation
-		elif self.special_chars == False:
-			stringSpecial = ''
-
-		characters = stringMethod + stringNumbers + stringSpecial
 		for p in range(iterations):
 			output_password = ''
 			for c in range(self.length):
@@ -35,5 +44,8 @@ class password:
 
 # Test
 
-password1 = password(20, 'both', True, True) # password length = 20
-password1.generate_intricate(3) # generate a memeorable password (containing words and numbers) 3 times
+var1 = complexpass(20, 'both', False, False) # password length = 20, string method is lowercase, numbers are true and special characters are false
+var1.generate(3) # generate the random password 3 times
+
+var2 = simplepass(20, 'abcdefghijklmnopqrstuvwxyz0123467589') # password length = 20, string method is lowercase, numbers are true and special characters are false
+var2.generate(3) # generate the random password 3 times
